2fbd18c6f7fdbb7b4fc6272459d9aca677db8615
Nikolay Patov <nikolay.patov@gmail.com>
2024-03-29 18:46:31 +0300

    fix: exe7

---
+++ src/main/java/sprint8/exe7/AirportDatabase.java
@@ -0,0 +1,54 @@
+package sprint8.exe7;
+
+class AirportDatabase {
+    private static Airport vnukovo = new Airport(
+            "Внуково",
+            "Москва",
+            "MOSCOW          ",
+            "Europe/Moscow"
+    );
+
+    private static Airport pulkovo = new Airport(
+            "Пулково",
+            "Санкт-Петербург",
+            "SAINT-PETERSBURG",
+            "Europe/Moscow"
+    );
+
+    private static Airport vladivostok = new Airport(
+            "Владивосток",
+            "Владивосток",
+            "VLADIVOSTOK     ",
+            "Asia/Vladivostok"
+    );
+
+    private static Airport koltsovo = new Airport(
+            "Кольцово",
+            "Екатеринбург",
+            "YEKATERINBURG   ",
+            "Asia/Yekaterinburg"
+    );
+
+    public static Airport getAirportByCode(String airportCode) {
+        /* С помощью оператора switch case верните правильный аэропорт по его коду:
+         * VKO - vnukovo
+         * LED - pulkovo
+         * SVX - koltsovo
+         * VVO - vladivostok
+         * Для неверного кода пробросьте исключение.
+         */
+        Airport airport = null;
+        switch (airportCode){
+            case "VKO": airport = vnukovo;
+            break;
+            case "LED": airport = pulkovo;
+                break;
+            case "SVX": airport = koltsovo;
+                break;
+            case "VVO": airport = vladivostok;
+                break;
+            default: throw new IllegalStateException("Неизвестный код аэропорта:" + airportCode);
+        }
+        return airport;
+    }
+}
