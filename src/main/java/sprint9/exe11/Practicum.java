b248a6a442409e26bb946e2f6d018955cba9901c
NikolayPatov <nikolay.patov@gmail.com>
2024-04-13 13:03:09 +0300

    fix: exe10

---
+++ src/main/java/sprint9/exe11/Practicum.java
@@ -0,0 +1,47 @@
+package sprint9.exe11;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class Practicum {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        URI url = URI.create("https://functions.yandexcloud.net/d4ed1i6t3f80hf0p7mer?base=RUB&symbols=USD,EUR");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            // проверяем, успешно ли обработан запрос
+            if (response.statusCode() == 200) {
+                JsonElement jsonElement = JsonParser.parseString(response.body());
+                if(!jsonElement.isJsonObject()) { // проверяем, точно ли мы получили JSON-объект
+                    System.out.println("Ответ от сервера не соответствует ожидаемому.");
+                    return;
+                }
+                // получите курс доллара и евро и запишите в переменные rateUSD и rateEUR
+                JsonElement rates = jsonElement.getAsJsonObject().get("rates");
+                double rateUSD = rates.getAsJsonObject().get("USD").getAsDouble();
+                double rateEUR = rates.getAsJsonObject().get("EUR").getAsDouble();
+
+                System.out.println("Стоимость рубля в долларах: " + rateUSD + " USD");
+                System.out.println("Стоимость рубля в евро: " + rateEUR + " EUR");
+            } else {
+                System.out.println("Что-то пошло не так. Сервер вернул код состояния: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) { // обрабатываем ошибки отправки запроса
+            System.out.println("Во время выполнения запроса возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+        }
+    }
+}
